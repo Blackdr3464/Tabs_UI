d84c0f8563d9bb6835d89f29501d93aee1f94915
Blackdr3464 <63839741+Blackdr3464@users.noreply.github.com>
2021-07-20 23:37:07 +0700

    add new

--- event.js
+++ event.js
@@ -1,6 +1,12 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
+const tabActive = $('.tab-item.active')
+const line = $('.tabs .line')
+
+line.style.left = tabActive.offsetLeft + 'px'
+line.style.width = tabActive.offsetWidth + 'px'
+
 const tabs = $$('.tab-item')
 const panes = $$('.tab-pane')
 
@@ -10,8 +16,10 @@ tabs.forEach((tab, index) => {
         $('.tab-item.active').classList.remove('active')
         $('.tab-pane.active').classList.remove('active')
 
+        line.style.left = this.offsetLeft + 'px'
+        line.style.width = this.offsetWidth + 'px'
+
         this.classList.add('active')
         pane.classList.add('active')
-
     }
 })
